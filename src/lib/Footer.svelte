e6d97a893f988578cec9a37051f4dfac81fbd7d3
Mijobar <Mijobar@aim.com>
2022-06-02 14:41:47 -0400

    Update Footer.svelte

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -103,14 +103,14 @@
 			{/each}
 		</ul>
 	</div>
-	<!-- PLEASE DO NOT REMOVE THE COPYRIGHT -->
+	<!-- PLEASE DO NOT REMOVE THE COPYRIGHT
 	<span class="copyright">&copy; 2021 - {year} <a href="https://github.com/nmelhado/league-page">League Page</a></span>
-	<br />
-	<!-- PLEASE DO NOT REMOVE THE BUILT BY -->
-	<span class="creator">Built by <a href="http://www.nmelhado.com/">Nicholas Melhado</a><br /></span>
+	<br /> -->
+	<!-- PLEASE DO NOT REMOVE THE BUILT BY
+	<span class="creator">Built by <a href="http://www.nmelhado.com/">Nicholas Melhado</a><br /></span> -->
 	<!-- You can remove the donation link (although any donations to help
-	 maintain and enhance League Page would be greatly appreciated!) -->
-	Love League Page? Please consider <a href="https://www.buymeacoffee.com/nmelhado">donating</a> to support enhancements or just to say thank you!
+	 maintain and enhance League Page would be greatly appreciated!) 
+	Love League Page? Please consider <a href="https://www.buymeacoffee.com/nmelhado">donating</a> to support enhancements or just to say thank you! -->
 </footer>
 
 <footer class="{!outOfDate ? '' : 'invisible'}" bind:this={el}>
@@ -127,12 +127,12 @@
 			{/each}
 		</ul>
 	</div>
-	<!-- PLEASE DO NOT REMOVE THE COPYRIGHT -->
+	<!-- PLEASE DO NOT REMOVE THE COPYRIGHT 
 	<span class="copyright">&copy; 2021 - {year} <a href="https://github.com/nmelhado/league-page">League Page</a></span>
-	<br />
-	<!-- PLEASE DO NOT REMOVE THE BUILT BY -->
-	<span class="creator">Built by <a href="http://www.nmelhado.com/">Nicholas Melhado</a><br /></span>
+	<br /> -->
+	<!-- PLEASE DO NOT REMOVE THE BUILT BY 
+	<span class="creator">Built by <a href="http://www.nmelhado.com/">Nicholas Melhado</a><br /></span>-->
 	<!-- You can remove the donation link (although any donations to help
-	 maintain and enhance League Page would be greatly appreciated!) -->
-	Love League Page? Please consider <a href="https://www.buymeacoffee.com/nmelhado">donating</a> to support enhancements or just to say thank you!
+	 maintain and enhance League Page would be greatly appreciated!) 
+	Love League Page? Please consider <a href="https://www.buymeacoffee.com/nmelhado">donating</a> to support enhancements or just to say thank you! -->
 </footer>
