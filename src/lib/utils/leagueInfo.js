691036db13ee67a39b3846d0f646d88e3c697977
Mijobar <Mijobar@aim.com>
2023-07-05 21:51:40 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "804762052366286848"; // your league ID
+export const leagueID = "970018429648384000"; // your league ID
 export const leagueName = "PCFL"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
